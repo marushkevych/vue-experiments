e27861483cafa97a4db49980af82ae4594820229
Marushkevych <marushkevych@gmail.com>
2018-04-05 13:34:56 -0400

    vew basics

---
+++ main.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Hover your mouse over me for a few seconds to see my dynamically bound title!',
+    title: 'You loaded this page on ' + new Date().toLocaleString(),
+    show: true
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    newTodo: '',
+    todos: [
+      {text: 'foo'},
+      {text: 'bar'}
+    ]
+  },
+  methods: {
+    add: function() {
+      if(this.newTodo) {
+        this.todos.push({text: this.newTodo})
+        this.newTodo = ''
+      }
+    }
+  }
+})
